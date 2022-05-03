76af5ed5d89256ab84457917413cb2c39fdcc9b1
Roman <ro.kovtunov@mail.ru>
2022-05-03 17:59:50 +0300

    First Commit

---
+++ src/main/java/ReaderFile.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Arrays;
+
+public class ReaderFile {
+    public static void ReadingFile() {
+        try (FileReader in = new FileReader(Solution.src); BufferedReader bufferReader = new BufferedReader(in)) {
+            while (bufferReader.ready())                                            //Читаем из файла по 1 строке
+            {
+                String line = bufferReader.readLine();
+                if (line.startsWith("alter")) {
+                    continue;
+                }                                                                   //Пропустили первую строчку с заголовками
+
+                String[] result = line.split("\t");                           //Разделили по табу, записали в массив
+                System.arraycopy(result, 0, Solution.arrayOfCSVFile, 0, Solution.arrayOfCSVFile.length);
+
+                DataSampling.DataSamplingFile();
+
+
+            }
+        } catch (Exception e) {
+        }
+
+    }}
